0e207117391cdd86fccab9608c8fb2a6e010e35e
gcoter <guillaume.coter@gmail.com>
2023-05-18 19:35:28 +0200

    Rename CommanderGameMode to DefaultGameMode

--- magic_the_gathering/game_modes/default.py
+++ magic_the_gathering/game_modes/default.py
@@ -1,10 +1,10 @@
 from magic_the_gathering.game_modes.base import GameMode
 
 
-class CommanderGameMode(GameMode):
+class DefaultGameMode(GameMode):
     def __init__(self):
         super().__init__(
-            name="Commander Game Mode",
+            name="Default Game Mode",
             initial_life_points=20,
             initial_hand_size=7,
         )
