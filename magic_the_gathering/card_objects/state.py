8d866ce352ee50ae24422d98804896e8088291f9
gcoter <guillaume.coter@gmail.com>
2023-05-19 22:30:22 +0200

    Add started_turn_controlled_by_player_id to CardObjectState  (#7)

--- magic_the_gathering/card_objects/state.py
+++ magic_the_gathering/card_objects/state.py
@@ -6,6 +6,8 @@ class CardObjectState:
         self,
         is_tapped: Optional[bool] = False,
         owner_player_id: Optional[int] = 0,
+        started_turn_controlled_by_player_id: Optional[int] = 0,
     ):
         self.is_tapped = is_tapped
         self.owner_player_id = owner_player_id
+        self.started_turn_controlled_by_player_id = started_turn_controlled_by_player_id
