df3524f9407c0f048bd4ee6f4af0566534ca2bf3
gcoter <guillaume.coter@gmail.com>
2023-05-22 00:42:28 +0200

    Add a BeginningPhase

---
+++ magic_the_gathering/phases/beginning.py
@@ -0,0 +1,11 @@
+from magic_the_gathering.game_state import GameState
+from magic_the_gathering.phases.base import Phase
+
+
+class BeginningPhase(Phase):
+    def __init__(self):
+        super().__init__(name="Beginning Phase")
+
+    def run(self, game_state: GameState) -> GameState:
+        game_state.current_player_has_played_a_land_this_turn = False
+        return game_state
