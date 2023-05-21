223ebfd5d11ad266fcded02a98b69a510f8c7d02
gcoter <guillaume.coter@gmail.com>
2023-05-21 18:57:43 +0200

    Remove methods of GameState (replaced with Action classes), reimplement zones

--- magic_the_gathering/game_state.py
+++ magic_the_gathering/game_state.py
@@ -1,9 +1,19 @@
-from random import shuffle
-from typing import List, Optional
+from collections import OrderedDict
+from enum import Enum
+from typing import Dict, List, Optional
 
 from magic_the_gathering.cards.base import Card
 from magic_the_gathering.game_modes.base import GameMode
 from magic_the_gathering.players.base import Player
+
+
+class ZonePosition(Enum):
+    HAND = 0
+    LIBRARY = 1
+    BOARD = 2
+    GRAVEYARD = 3
+    EXILE = 4
+    STACK = 5
 
 
 class GameState:
@@ -14,12 +24,7 @@ class GameState:
         current_turn_counter: Optional[int] = 0,
         current_player_index: Optional[int] = 0,
         current_player_has_played_a_land_this_turn: Optional[bool] = False,
-        hands: Optional[List[List[Card]]] = None,
-        libraries: Optional[List[List[Card]]] = None,
-        boards: Optional[List[List[Card]]] = None,
-        graveyards: Optional[List[List[Card]]] = None,
-        exiles: Optional[List[Card]] = None,
-        stack: Optional[List[Card]] = None,
+        zones: Optional[Dict[ZonePosition, List[OrderedDict[str, Card]]]] = None,
     ):
         self.game_mode = game_mode
         self.players = players
@@ -28,12 +33,23 @@ class GameState:
         # TODO: Should 'current_player_has_played_a_land_this_turn' be a property of the player, not of the game state?
         # TODO: At some point we'll change this so that we allow a player to have n land drops, 1 by default, but can change with some cards
         self.current_player_has_played_a_land_this_turn = current_player_has_played_a_land_this_turn
-        self.hands = hands
-        self.libraries = libraries
-        self.boards = boards
-        self.graveyards = graveyards
-        self.exiles = exiles
-        self.stack = stack
+        self.zones = zones
+        if self.zones is None:
+            self.zones = {
+                ZonePosition.HAND: [OrderedDict() for _ in range(self.n_players)],
+                ZonePosition.LIBRARY: [OrderedDict() for _ in range(self.n_players)],
+                ZonePosition.BOARD: [OrderedDict() for _ in range(self.n_players)],
+                ZonePosition.GRAVEYARD: [OrderedDict() for _ in range(self.n_players)],
+                ZonePosition.EXILE: [OrderedDict() for _ in range(self.n_players)],
+                ZonePosition.STACK: OrderedDict(),  # FIXME: It should probably be just a list but it is simpler to use the same class as the other zones for now
+            }
+        self.__assert_zones_validity()
+
+    def __assert_zones_validity(self):
+        for zone in ZonePosition:
+            assert zone in self.zones
+            assert len(self.zones[zone]) == self.n_players
+        assert "stack" in self.zones
 
     @property
     def n_players(self):
@@ -42,61 +58,3 @@ class GameState:
     @property
     def current_player(self):
         return self.players[self.current_player_index]
-
-    def initialize_for_new_game(self):
-        # TODO: It assumes that the players were initialized, is it a good idea?
-        # FIXME: The game mode contains information about the player initial life points and hand size, but it is not used here
-        # TODO: Pass the current player index? (for now it is always 0)
-        # Set hands, libraries, boards and stack to empty lists
-        self.hands = [[] for _ in range(self.n_players)]
-        self.libraries = [[] for _ in range(self.n_players)]
-        self.boards = [[] for _ in range(self.n_players)]
-        self.graveyards = [[] for _ in range(self.n_players)]
-        # TODO: Is there one exile per player?
-        self.exiles = [[] for _ in range(self.n_players)]
-        self.stack = []
-
-    def draw_cards_from_deck(self, player_index, n_cards=1):
-        n_cards = min(n_cards, len(self.libraries[player_index]))
-        drawn_cards = [self.libraries[player_index].pop(0) for _ in range(n_cards)]
-        self.hands[player_index].extend(drawn_cards)
-
-    def add_all_hand_cards_to_deck(self, player_index):
-        self.libraries[player_index].extend(self.hands[player_index])
-        self.hands[player_index] = []
-
-    def shuffle_deck(self, player_index):
-        shuffle(self.libraries[player_index])
-
-    def shuffle_all_decks(self):
-        for player_index in range(len(self.libraries)):
-            self.shuffle_deck(player_index=player_index)
-
-    def set_decks(self, decks: List[List[Card]]):
-        self.libraries = decks
-
-    def play_card(self, player_index, hand_card_index):
-        card = self.hands[player_index].pop(hand_card_index)
-        card.is_tapped = False
-        self.boards[card.owner_player_index].append(card)
-
-    def cast_card(self, player_index, hand_card_index):
-        card = self.hands[player_index].pop(hand_card_index)
-        self.stack.append(card)
-
-    def resolve_top_of_the_stack(self):
-        # TODO: Currently, this was implemented with casting creatures or lands in mind. It should be generalized.
-        card = self.stack.pop(-1)
-        card.is_tapped = False
-        self.boards[card.owner_player_index].append(card)
-
-    def untap_all_permanents(self, player_index: int):
-        for permanent in self.boards[player_index]:
-            permanent.is_tapped = False
-
-    def tap_cards(self, player_index, board_indices):
-        for board_index in board_indices:
-            self.boards[player_index][board_index].is_tapped = True
-
-    def pass_to_next_player(self):
-        self.current_player_index = (self.current_player_index + 1) % self.n_players
