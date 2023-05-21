fe1ec5c858f608b9a7303fb2c016400695311b16
gcoter <guillaume.coter@gmail.com>
2023-05-21 18:55:37 +0200

    Remove Player methods, add mana_pool attribute

--- magic_the_gathering/players/base.py
+++ magic_the_gathering/players/base.py
@@ -1,38 +1,19 @@
-from typing import List, Tuple, Union
-
-from magic_the_gathering.game_state import GameState
+from typing import Dict
 
 
 class Player:
     def __init__(
         self,
         life_points: int = 20,
+        mana_pool: Dict[str, int] = None,
     ):
         self.life_points = life_points
-
-    def choose_mulligan(self, game_state: GameState) -> bool:
-        pass
-
-    def choose_land_to_play(self) -> Union[int, None]:
-        pass
-
-    def choose_creatures_to_cast(self) -> List[int]:
-        pass
-
-    def choose_artifacts_to_cast(self):
-        pass
-
-    def choose_enchantment_to_cast(self):
-        pass
-
-    def choose_sorceries_to_cast(self):
-        pass
-
-    def choose_instants_to_cast(self):
-        pass
-
-    def choose_attackers(self) -> Tuple[List[int], List[int]]:
-        pass
-
-    def choose_blockers(self) -> Tuple[List[int], List[int]]:
-        pass
+        self.mana_pool = mana_pool
+        if self.mana_pool is None:
+            self.mana_pool = {
+                "white": 0,
+                "blue": 0,
+                "black": 0,
+                "red": 0,
+                "green": 0,
+            }
