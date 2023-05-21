5a8cfbf3d18690b0517fb19e4c4c09e9c5b5e7a8
gcoter <guillaume.coter@gmail.com>
2023-05-22 00:45:15 +0200

    Add attribute is_alive and method choose_action to Player

--- magic_the_gathering/players/base.py
+++ magic_the_gathering/players/base.py
@@ -1,12 +1,12 @@
-from typing import Dict
+from abc import abstractmethod
+from typing import Dict, List
+
+from magic_the_gathering.actions.base import Action
+from magic_the_gathering.game_state import GameState
 
 
 class Player:
-    def __init__(
-        self,
-        life_points: int = 20,
-        mana_pool: Dict[str, int] = None,
-    ):
+    def __init__(self, life_points: int = 20, mana_pool: Dict[str, int] = None, is_alive: bool = True):
         self.life_points = life_points
         self.mana_pool = mana_pool
         if self.mana_pool is None:
@@ -17,3 +17,8 @@ class Player:
                 "red": 0,
                 "green": 0,
             }
+        self.is_alive = is_alive  # FIXME: Maybe is_alive is just the same as life_points > 0?
+
+    @abstractmethod
+    def choose_action(self, game_state: GameState, possible_actions: List[Action]) -> Action:
+        raise NotImplementedError
