75ed11bd70f0a7d03bbf0f3e3c9224e64d5da868
gcoter <guillaume.coter@gmail.com>
2023-05-22 22:12:00 +0200

    Implement CombatDamagePhase

--- magic_the_gathering/phases/combat_damage.py
+++ magic_the_gathering/phases/combat_damage.py
@@ -1,3 +1,4 @@
+from magic_the_gathering.actions.deal_damage import DealDamageAction
 from magic_the_gathering.game_state import GameState
 from magic_the_gathering.phases.base import Phase
 
@@ -7,6 +8,7 @@ class CombatDamagePhase(Phase):
         super().__init__(name="Combat: Damage Phase")
 
     def run(self, game_state: GameState) -> GameState:
-        # We assume the blockers are already ordered as chosen by the attacker
-        # TODO: Implement this
+        # FIXME: We assume the blockers are already ordered as chosen by the attacker
+        for deal_damage_action in DealDamageAction.list_possible_actions(game_state):
+            game_state = deal_damage_action.execute(game_state)
         return game_state
