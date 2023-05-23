5caee43fc74229123f491285e9b3efea2d8cf538
gcoter <guillaume.coter@gmail.com>
2023-05-23 20:25:17 +0200

    Add FIXME in DrawPhase

--- magic_the_gathering/phases/draw.py
+++ magic_the_gathering/phases/draw.py
@@ -8,5 +8,6 @@ class DrawPhase(Phase):
         super().__init__(name="Draw Phase")
 
     def _run(self, game_state: GameState) -> GameState:
+        # FIXME: Draw should do nothing for the first player at the beginning of the game
         action = DrawAction(owner="game", player_index=game_state.current_player_index)
         return action.execute(game_state)
