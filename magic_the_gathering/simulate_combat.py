f9f4e87abbefe29d6ac193be3b8f02b2e46f3b6b
gcoter <guillaume.coter@gmail.com>
2023-05-25 22:56:45 +0200

    Simulate 100 combats with 7 attackers and 7 potential blockers

--- magic_the_gathering/simulate_combat.py
+++ magic_the_gathering/simulate_combat.py
@@ -1,6 +1,6 @@
-import json
 import logging
 from collections import OrderedDict
+from time import time
 
 import pandas as pd
 
@@ -25,6 +25,11 @@ def simulate_combat():
     player_0_board_scryfall_ids = [
         "00223901-d462-41b0-9749-b093058f682f",  # Coral Eel
         "065d497d-5cfd-43c9-8c86-9a1da3d7e17e",  # Seagraf Skaab
+        "00d89839-60d7-4de2-a78a-1afdcc21c053",  # Tolarian Scholar
+        "0453fac7-10b9-49d7-8219-78faf9d92d0c",  # Fugitive Wizard
+        "09ef366b-26f5-473a-ab96-e668ed54d691",  # Coral Merfolk
+        "0f244233-f2e8-48f8-9106-e7cd186efd51",  # Naga Eternal
+        "126fec7a-4f36-49e5-a2d7-96deb7af856f",  # Merfolk of the Pearl Trident
     ]
     player_0_board = OrderedDict()
     for scryfall_id in player_0_board_scryfall_ids:
@@ -40,6 +45,11 @@ def simulate_combat():
     player_1_board_scryfall_ids = [
         "0d3eff55-6a14-4c01-8b05-715094a319b3",  # Mons's Goblin Raiders
         "22955bf2-4119-40dd-a9de-25d875a93cd5",  # Hill Giant
+        "11bf2cc0-799f-4eb8-b338-ed7543f469e7",  # Gray Ogre
+        "13696657-aeef-4add-9a3b-8137fce01fe3",  # Crimson Kobolds
+        "14a83031-8b57-41d2-b586-bb4dcf16136a",  # Trained Orgg
+        "1d21c8c9-6e16-4eb2-b2f5-3998f0f958ae",  # Goblin Hero
+        "1dce786c-af9c-49f5-b372-741458180a09",  # Hyena Pack
     ]
     player_1_board = OrderedDict()
     for scryfall_id in player_1_board_scryfall_ids:
@@ -74,4 +84,9 @@ def simulate_combat():
 
 
 if __name__ == "__main__":
-    simulate_combat()
+    elapsed_times = []
+    for _ in range(100):
+        start_time = time()
+        simulate_combat()
+        elapsed_times.append(time() - start_time)
+    print(f"Average elapsed time: {sum(elapsed_times) / len(elapsed_times)} seconds")
