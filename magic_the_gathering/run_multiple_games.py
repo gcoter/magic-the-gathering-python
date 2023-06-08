ae575fcdc0a26c0bf9df6048a60e87aef9e8e8f7
gcoter <guillaume.coter@gmail.com>
2023-06-08 21:51:11 +0200

    Rename run_n_games to run_multiple_games

--- magic_the_gathering/run_multiple_games.py
+++ magic_the_gathering/run_multiple_games.py
@@ -1,10 +1,10 @@
 from magic_the_gathering.run_one_game import run_one_game
 
 
-def run_n_games(n_games: int = 100):
+def run_multiple_games(n_games: int = 100):
     for _ in range(n_games):
         run_one_game(n_players=2)
 
 
 if __name__ == "__main__":
-    run_n_games(n_games=10)
+    run_multiple_games(n_games=10)
