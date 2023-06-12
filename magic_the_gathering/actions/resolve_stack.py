0802ec982157f21e366304f9a11b5ccf544a9976
gcoter <guillaume.coter@gmail.com>
2023-06-12 22:36:35 +0200

    Fix ResolveTopOfStackAction

--- magic_the_gathering/actions/resolve_stack.py
+++ magic_the_gathering/actions/resolve_stack.py
@@ -15,7 +15,9 @@ class ResolveTopOfStackAction(Action):
     def _execute(self, game_state: GameState) -> GameState:
         stack = game_state.zones[ZonePosition.STACK]
         assert len(stack) > 0
-        resolved_card = stack.pop(-1)
+        resolved_card_uuid = next(reversed(stack))
+        resolved_card = stack[resolved_card_uuid]
+        del stack[resolved_card_uuid]
         if resolved_card.is_permanent:
             assert resolved_card.state is not None
             player_board = game_state.zones[ZonePosition.BOARD][resolved_card.state.owner_player_id]
