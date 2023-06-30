3570f6b13a161710c0b29d332ae39bc1259902b6
gcoter <guillaume.coter@gmail.com>
2023-06-30 23:17:13 +0200

    Modify preprocessor and dataset to return source/target card vectors as well

--- magic_the_gathering/players/deep_learning_based/single_action_scorer/dataset.py
+++ magic_the_gathering/players/deep_learning_based/single_action_scorer/dataset.py
@@ -11,19 +11,36 @@ class SingleActionScorerPreprocessor:
         self.device = device
 
     def preprocess(
-        self, game_state_vectors: Dict[str, np.ndarray], action_vectors: Dict[str, np.ndarray], max_n_cards: int
+        self,
+        game_state_vectors: Dict[str, np.ndarray],
+        action_vectors: Dict[str, np.ndarray],
+        max_n_zone_vectors: int,
+        zone_vector_dim: int,
+        max_n_action_source_cards: int,
+        max_n_action_target_cards: int,
     ) -> Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]]:
         preprocessed_game_state_vectors = self.preprocess_game_state_vectors(
-            game_state_vectors, max_n_cards=max_n_cards
+            game_state_vectors, max_n_zone_vectors=max_n_zone_vectors, zone_vector_dim=zone_vector_dim
         )
-        preprocessed_action_vectors = self.preprocess_action_vectors(action_vectors, max_n_cards=max_n_cards)
+        preprocessed_action_vectors = self.preprocess_action_vectors(
+            action_vectors,
+            max_n_zone_vectors=max_n_zone_vectors,
+            zone_vector_dim=zone_vector_dim,
+            max_n_action_source_cards=max_n_action_source_cards,
+            max_n_action_target_cards=max_n_action_target_cards,
+        )
         return preprocessed_game_state_vectors, preprocessed_action_vectors
 
     def preprocess_game_state_vectors(
-        self, game_state_vectors: Dict[str, np.ndarray], max_n_cards: int
+        self,
+        game_state_vectors: Dict[str, np.ndarray],
+        max_n_zone_vectors: int,
+        zone_vector_dim: int,
     ) -> Dict[str, torch.Tensor]:
         zones_vectors = game_state_vectors["zones"]
-        padded_zones_vectors, zones_padding_mask = self.__pad_to_max_zone_length(zones_vectors, max_n_cards=max_n_cards)
+        padded_zones_vectors, zones_padding_mask = self.__pad_2_dim_vectors(
+            zones_vectors, target_dim_0=max_n_zone_vectors, target_dim_1=zone_vector_dim
+        )
         return {
             "global": torch.from_numpy(game_state_vectors["global"]).float().to(self.device),
             "players": torch.from_numpy(game_state_vectors["players"]).float().to(self.device),
@@ -32,48 +49,103 @@ class SingleActionScorerPreprocessor:
         }
 
     def preprocess_action_vectors(
-        self, action_vectors: Dict[str, np.ndarray], max_n_cards: int
+        self,
+        action_vectors: Dict[str, np.ndarray],
+        max_n_zone_vectors: int,
+        zone_vector_dim: int,
+        max_n_action_source_cards: int,
+        max_n_action_target_cards: int,
     ) -> Dict[str, torch.Tensor]:
+        source_card_vectors = action_vectors["source_card_vectors"]
+        padded_source_card_vectors, source_card_vectors_padding_mask = self.__pad_2_dim_vectors(
+            source_card_vectors, target_dim_0=max_n_action_source_cards, target_dim_1=zone_vector_dim
+        )
+
         source_card_uuids = action_vectors["source_card_uuids"]
-        padded_source_card_uuids, source_card_uuids_padding_mask = self.__pad_to_max_zone_length(
-            source_card_uuids, max_n_cards=max_n_cards
+        padded_source_card_uuids, source_card_uuids_padding_mask = self.__pad_1_dim_vectors(
+            source_card_uuids, target_dim_0=max_n_zone_vectors
+        )
+
+        target_card_vectors = action_vectors["target_card_vectors"]
+        padded_target_card_vectors, target_card_vectors_padding_mask = self.__pad_2_dim_vectors(
+            target_card_vectors, target_dim_0=max_n_action_target_cards, target_dim_1=zone_vector_dim
         )
 
         target_card_uuids = action_vectors["target_card_uuids"]
-        padded_target_card_uuids, target_card_uuids_padding_mask = self.__pad_to_max_zone_length(
-            target_card_uuids, max_n_cards=max_n_cards
+        padded_target_card_uuids, target_card_uuids_padding_mask = self.__pad_1_dim_vectors(
+            target_card_uuids, target_dim_0=max_n_zone_vectors
         )
 
         return {
             "general": torch.from_numpy(action_vectors["general"]).float().to(self.device),
+            "source_card_vectors": torch.from_numpy(padded_source_card_vectors).float().to(self.device),
+            "source_card_vectors_padding_mask": torch.from_numpy(source_card_vectors_padding_mask)
+            .bool()
+            .to(self.device),
             "source_card_uuids": torch.from_numpy(padded_source_card_uuids).bool().to(self.device),
             "source_card_uuids_padding_mask": torch.from_numpy(source_card_uuids_padding_mask).bool().to(self.device),
+            "target_card_vectors": torch.from_numpy(padded_target_card_vectors).float().to(self.device),
+            "target_card_vectors_padding_mask": torch.from_numpy(target_card_vectors_padding_mask)
+            .bool()
+            .to(self.device),
             "target_card_uuids": torch.from_numpy(padded_target_card_uuids).bool().to(self.device),
             "target_card_uuids_padding_mask": torch.from_numpy(target_card_uuids_padding_mask).bool().to(self.device),
         }
 
-    def __pad_to_max_zone_length(self, vectors: np.ndarray, max_n_cards: int) -> Tuple[np.ndarray, np.ndarray]:
-        assert len(vectors) > 0
-        assert max_n_cards >= len(vectors)
-        padding_mask = np.zeros(max_n_cards)
-        padding_mask[: len(vectors)] = 1
-        if len(vectors.shape) == 1:
-            pad_width = ((0, max_n_cards - len(vectors)),)
-        elif len(vectors.shape) == 2:
-            pad_width = ((0, max_n_cards - len(vectors)), (0, 0))
+    def __pad_1_dim_vectors(self, vectors: np.ndarray, target_dim_0: int):
+        if len(vectors) == 0:
+            padded_vectors = np.zeros(target_dim_0)
+            padding_mask = np.ones(target_dim_0)
+            return padded_vectors, padding_mask
+        if len(vectors) < target_dim_0:
+            pad_width = ((0, target_dim_0 - len(vectors)),)
+            padding_mask = np.zeros(target_dim_0)
+            padding_mask[: len(vectors)] = 1
+            padded_vectors = np.pad(vectors, pad_width=pad_width)
+            return padded_vectors, padding_mask
         else:
-            raise ValueError(f"Invalid vector shape: {vectors.shape}")
-        padded_vectors = np.pad(vectors, pad_width=pad_width)
-        return padded_vectors, padding_mask
+            padded_vectors = vectors[:target_dim_0]
+            padding_mask = np.zeros(target_dim_0)
+            return padded_vectors, padding_mask
+
+    def __pad_2_dim_vectors(self, vectors: np.ndarray, target_dim_0: int, target_dim_1: int):
+        if len(vectors) == 0:
+            padded_vectors = np.zeros(shape=(target_dim_0, target_dim_1))
+            padding_mask = np.ones(target_dim_0)
+            return padded_vectors, padding_mask
+        assert len(vectors.shape) == 2
+        assert vectors.shape[1] == target_dim_1
+        if len(vectors) < target_dim_0:
+            pad_width = ((0, target_dim_0 - len(vectors)), (0, 0))
+            padding_mask = np.zeros(target_dim_0)
+            padding_mask[: len(vectors)] = 1
+            padded_vectors = np.pad(vectors, pad_width=pad_width)
+            return padded_vectors, padding_mask
+        else:
+            padded_vectors = vectors[:target_dim_0]
+            padding_mask = np.zeros(target_dim_0)
+            return padded_vectors, padding_mask
 
 
 class SingleActionScorerDataset(torch.utils.data.Dataset):
-    def __init__(self, device, max_n_cards: int, game_logs_dataset: GameLogsDataset, return_label: bool = False):
+    def __init__(
+        self,
+        device,
+        max_n_zone_vectors: int,
+        zone_vector_dim: int,
+        max_n_action_source_cards: int,
+        max_n_action_target_cards: int,
+        game_logs_dataset: GameLogsDataset,
+        return_label: bool = False,
+    ):
         self.device = device
         self.game_logs_dataset = game_logs_dataset
         self.return_label = return_label
         self.index_df = self.game_logs_dataset.get_index()
-        self.max_n_cards = max_n_cards
+        self.max_n_zone_vectors = max_n_zone_vectors
+        self.zone_vector_dim = zone_vector_dim
+        self.max_n_action_source_cards = max_n_action_source_cards
+        self.max_n_action_target_cards = max_n_action_target_cards
         self.preprocessor = SingleActionScorerPreprocessor(device=self.device)
 
     def __len__(self) -> int:
@@ -88,7 +160,12 @@ class SingleActionScorerDataset(torch.utils.data.Dataset):
         chosen_action_vectors = item_dict["possible_actions"][chosen_action_index]
 
         preprocessed_game_state_vectors, preprocessed_action_vectors = self.preprocessor.preprocess(
-            game_state_vectors=game_state_vectors, action_vectors=chosen_action_vectors, max_n_cards=self.max_n_cards
+            game_state_vectors=game_state_vectors,
+            action_vectors=chosen_action_vectors,
+            max_n_zone_vectors=self.max_n_zone_vectors,
+            zone_vector_dim=self.zone_vector_dim,
+            max_n_action_source_cards=self.max_n_action_source_cards,
+            max_n_action_target_cards=self.max_n_action_target_cards,
         )
 
         if self.return_label:
