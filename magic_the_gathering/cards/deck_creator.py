1d629b099a1ca8e8480b9b1a9e3d72eec8bba365
gcoter <guillaume.coter@gmail.com>
2023-07-15 14:59:25 +0200

    Remove deprecated data, notebooks and class

--- magic_the_gathering/cards/deck_creator.py
+++ magic_the_gathering/cards/deck_creator.py
@@ -1,7 +1,7 @@
 import abc
 import random
 from collections import OrderedDict
-from typing import List, Optional
+from typing import List
 
 import pandas as pd
 
@@ -12,43 +12,6 @@ class DeckCreator:
     @abc.abstractmethod
     def create_decks(self, n_players: int) -> List[OrderedDict[str, Card]]:
         pass
-
-
-class JumpstartDeckCreator:
-    def __init__(
-        self,
-        card_sets_df: pd.DataFrame,
-        cards_df: pd.DataFrame,
-        allowed_types: Optional[List[str]] = None,
-    ) -> None:
-        self.card_sets_df = card_sets_df
-        self.cards_df = cards_df
-        self.allowed_types = allowed_types
-
-    def create_decks(self, n_players: int) -> List[OrderedDict[str, Card]]:
-        decks = []
-        for _ in range(n_players):
-            current_deck = OrderedDict()
-            for _ in range(2):
-                chosen_deck_name = random.choice(self.card_sets_df["deck_name"].unique())
-                deck_composition_df = self.card_sets_df[self.card_sets_df["deck_name"] == chosen_deck_name]
-                for _, row in deck_composition_df.iterrows():
-                    card_name = row["card_name"]
-                    card_count = row["card_count"]
-                    if card_name in self.cards_df["name"].values:
-                        card_series = self.cards_df[self.cards_df["name"] == card_name].iloc[0]
-                        if self.allowed_types is not None:
-                            card_types = card_series["type_line"].split(" — ")[0].split()
-                            if not any(card_type in self.allowed_types for card_type in card_types):
-                                continue
-                        card = Card.from_series(card_series)
-                        cards_to_add = [card] * card_count
-                        for card in cards_to_add:
-                            current_deck[card.uuid] = card
-                    else:
-                        print(f"Card '{card_name}' not found in cards dataframe")
-            decks.append(current_deck)
-        return decks
 
 
 class RandomVanillaDeckCreator:
